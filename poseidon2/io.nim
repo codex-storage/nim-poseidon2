dbe3d8a692b1d6852a3d41675b7320a5a477d708
Mark Spanbroek <mark@spanbroek.net>
2023-11-02 10:07:45 +0100

    Rename marshal -> toBytes, unmarshal -> fromBytes
    For two reasons:
    - to distinguish them from the marshalling functions
      from constantine
    - they do not follow the convention that something
      that is marshalled can be unmarshalled, because
      they take in 31 bytes but produce 32 bytes

--- poseidon2/io.nim
+++ poseidon2/io.nim
@@ -3,7 +3,7 @@ import constantine/math/arithmetic
 import constantine/math/io/io_bigints
 import constantine/math/config/curves
 
-func unmarshal*(_: type F, bytes: openArray[byte]): F =
+func fromBytes*(_: type F, bytes: openArray[byte]): F =
   ## Converts bytes into a field element. The byte array is interpreted as a
   ## canonical little-endian big integer. The array should be of length 31 bytes
   ## or less to ensure that it fits in a field of 254 bits. The remaining
@@ -14,20 +14,20 @@ func unmarshal*(_: type F, bytes: openArray[byte]): F =
   let bigint = B.unmarshal(padded, littleEndian)
   return F.fromBig(bigint)
 
-func unmarshal*(_: type seq[F], bytes: openArray[byte]): seq[F] =
+func fromBytes*(_: type seq[F], bytes: openArray[byte]): seq[F] =
   ## Converts bytes into field elements. The byte array is converted 31 bytes at
-  ## a time with the `F.unmarshal()` function.
+  ## a time with the `F.fromBytes()` function.
   const chunkLen = 31
   var elements: seq[F]
   var chunkStart = 0
   while chunkStart < bytes.len:
     let chunkEnd = min(chunkStart + 31, bytes.len)
-    let element = F.unmarshal(bytes.toOpenArray(chunkStart, chunkEnd - 1))
+    let element = F.fromBytes(bytes.toOpenArray(chunkStart, chunkEnd - 1))
     elements.add(element)
     chunkStart += chunkLen
   return elements
 
-func marshal*(element: F): array[32, byte] =
+func toBytes*(element: F): array[32, byte] =
   ## Converts a field element into its canonical representation in little-endian
   ## byte order. Uses at most 254 bits, the remaining 6 most-significant bits
   ## are set to 0.
